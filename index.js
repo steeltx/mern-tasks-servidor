ce824683663eacc94ce92f4add955a2c9b6e98bd
steeltx <realsteel.gnu@gmail.com>
2021-02-11 22:17:20 -0600

    Conexión a MongoDB y configuraciones express

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+
+// crear el servidorobject
+const app = express();
+
+// conectar a la BD
+conectarDB();
+
+// puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// definir la pagina principal
+app.get('/',(req, res) => {
+    res.send('Hola Mundo');
+})
+
+// arranca la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+})
