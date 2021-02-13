971567220436f6c018cfcb998c76e444943dfb70
steeltx <realsteel.gnu@gmail.com>
2021-02-12 21:52:15 -0600

    API autenticación de usuarios

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+// rutas para autenticar usuarios
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+const authController = require('../controllers/authController');
+
+// crear un usuario
+router.post('/', 
+    [
+        check('email','Agrega un email válido').isEmail(),
+        check('password','El password debe ser minimo 6 caracteres').isLength({min: 6})
+    ],
+    authController.autenticarUsuario
+);
+
+module.exports = router;
