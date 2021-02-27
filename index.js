47751ae1f67dc32aedef7147651e14c17c22bc5b
steeltx <realsteel.gnu@gmail.com>
2021-02-26 21:59:43 -0600

    Ajustes para deploy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 // importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
 });
 
 // arranca la app
-app.listen(PORT, () => {
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+app.listen(port,'0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
